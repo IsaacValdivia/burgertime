822adf426331f4965ae9004f971ab2a0a7ec9d13
IsaacValdivia <isaac.valdivia.dnc@gmail.com>
2020-04-08 19:07:58 +0200

    comentarios hechos todos ya

--- src/Actor.hpp
+++ src/Actor.hpp
@@ -12,22 +12,43 @@ private:
     static constexpr auto sprite_scale = 2;
 
 protected:
-    bool alive;
+    bool alive; // Is the actor alive?.
 
-    Direction direction;
+    Direction direction; // Current direction.
     Direction mirror_state;
 
-    std::shared_ptr<Map> map;
+    std::shared_ptr<Map> map; // Pointer to map.
 
-    const BT_sprites::Sprite first_sprite;
+    const BT_sprites::Sprite first_sprite; // Initial sprite.
 
     void mirror();
 public:
+    /**
+     * [description]
+     * 
+     * @param init_pos 
+     * @param init_sprite 
+     * @param first_sprite 
+     * @param map 
+     */
     Actor(const sf::Vector2f &init_pos, const BT_sprites::Sprite init_sprite, const BT_sprites::Sprite first_sprite, std::shared_ptr<Map> map);
 
+    /**
+     * Returns a e
+     * 
+     * @return sf::FloatRect 
+     */
     sf::FloatRect getCollisionShape() const override;
 
+    /**
+     * Checks alive field
+     * 
+     * @return bool 
+     */
     bool is_alive() const;
 
+    /**
+     * Sets alive attribute to false
+     */
     virtual void die();
 };
